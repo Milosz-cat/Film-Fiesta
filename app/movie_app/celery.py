d3d78a0c8208b0621d22cef5264177c6193ef1d8
Milosz Bochenek <miloszbochenek20@gmail.com>
2023-09-05 22:10:43 +0200

    Fixing minor bug with celery

--- app/movie_app/celery.py
+++ app/movie_app/celery.py
@@ -10,10 +10,16 @@ app = Celery('movie_app')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-"""Define the beat schedule for periodic tasks. This configuration sets up three tasks to run at specified intervals."""
+"""
+Define the beat schedule for periodic tasks. This configuration sets up three tasks to run at specified intervals.
+- scrape_imdb_top_250: This task scrapes the IMDB Top 250 movies. It's scheduled to run every day at midnight.
+- scrape_oscar_best_picture: 
+    This task scrapes the Oscar Best Picture winners.
+    It's scheduled to run at midnight on the 5th of March every year.
+    This is likely to coincide with the annual Oscar awards ceremony.
+"""
 app.conf.beat_schedule = {
     'scrape_imdb_top_250': {
-        """This task scrapes the IMDB Top 250 movies. It's scheduled to run every day at midnight."""
         'task': 'list_management.tasks.scrape_imdb_top_250',
         'schedule': crontab(minute=0, hour=0),  
     },
@@ -22,11 +28,6 @@ app.conf.beat_schedule = {
         'schedule': crontab(minute=5, hour=0),
     },
     'scrape_oscar_best_picture': {
-        """
-        This task scrapes the Oscar Best Picture winners.
-        It's scheduled to run at midnight on the 5th of March every year.
-        This is likely to coincide with the annual Oscar awards ceremony.
-        """
         'task': 'list_management.tasks.scrape_oscar_best_picture',
         'schedule': crontab(minute=0, hour=0, day_of_month='5', month_of_year='3'),
     }
